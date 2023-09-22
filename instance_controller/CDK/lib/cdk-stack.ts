7d1c374a4c3a8cbd081c3626ceaeb8377408e016
Josh Renshaw <josh_renshaw@live.com>
2023-09-22 17:49:02 +0800

    adding docs and cfdn + s3 iac

--- instance_controller/CDK/lib/cdk-stack.ts
+++ instance_controller/CDK/lib/cdk-stack.ts
@@ -1,16 +1,12 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { CloudFrontToS3 } from '@aws-solutions-constructs/aws-cloudfront-s3';
 
 export class CdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        new CloudFrontToS3(this, 'instance-controller-website-bucket', {});
+    }
 }
